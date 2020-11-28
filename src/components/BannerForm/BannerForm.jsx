39319511b13b2e0633d21d92157f5597c5039084
Elina <ella-abella@mail.ru>
2020-11-28 14:22:38 +0300

    add choice of shapes

--- src/components/BannerForm/BannerForm.jsx
+++ src/components/BannerForm/BannerForm.jsx
@@ -6,6 +6,11 @@ const BannerForm = () => {
     return (
         <div className="banner-form">
             <div className="banner-form__label">Shape</div>
+            <div className="shapes-container">
+                <div className="shapes-container__shape-square" />
+                <div className="shapes-container__shape-vertical" />
+                <div className="shapes-container__shape-gorizontal" />
+            </div>
             <div className="banner-form__label">Background color</div>
             <input className="banner-form__input" />
             <input className="banner-form__input" />
