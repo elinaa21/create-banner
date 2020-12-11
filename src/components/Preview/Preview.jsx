9de9458b9d4c0bac88c11cb76e260f8b74d4858b
Elina <ella-abella@mail.ru>
2020-12-11 13:10:51 +0300

    resize first line

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -7,14 +7,21 @@ import './Preview.scss';
 const Preview = () => {
     const shape = useSelector(state => state.banner.shape);
     const color = useSelector(state => state.banner.color);
-    const firstLine = useSelector(state => state.banner.firstLine);
+    const firstLineText = useSelector(state => state.banner.firstLine);
+    const firstFontSize = firstLineText.length > 20 ? 900 / firstLineText.length : 45;
 
     return (
         <div className="preview-zone">
             <PreviewHeader />
             <div className="banner-container">
                 <div className={`banner_${shape}`} style={{ background: color }}>
-                    <div>{firstLine}</div>
+                    <div
+                        className="banner__first-line"
+                        style={{ fontSize: firstFontSize }}
+                        id="firstLine"
+                    >
+                        {firstLineText}
+                    </div>
                 </div>
             </div>
         </div>
